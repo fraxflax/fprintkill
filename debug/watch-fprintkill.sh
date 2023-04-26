3613f33614587ef83fe0709d5e0c4a0975198a2f
fraxflax <frax@axnet.nu>
2023-04-26 15:25:27 +0200

    Update watch-fprintkill.sh

--- debug/watch-fprintkill.sh
+++ debug/watch-fprintkill.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 PGREP='pgrep fprintkill ; pgrep fprintd-verify'
 for c in $@; do PGREP="$PGREP ; pgrep $c"; done
 watch -n1 "(echo $$ ; eval \"$PGREP\") | xargs ps -oppid,pid,pgid,cmd "
