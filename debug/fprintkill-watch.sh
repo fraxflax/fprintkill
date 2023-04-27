df7438990f5b6ad5f64c7a70ae9f3d7a6ec6ece2
fraxflac <frax@axnet.nu>
2023-04-27 13:48:21 +0200

    replaced with fprintkill-watch-gp-and-cmd.sh

--- debug/fprintkill-watch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# example usage:
-# ./fprintkill-watch.sh xtrlock -f
-
-echo ~~~ /tmp/fprintkill-watch.$$.log ~~~ > /tmp/fprintkill-watch.$$.log
-( while ps $$ >/dev/null 2>&1; do sleep 1; done ; rm -v /tmp/fprintkill-watch.$$.log ) &
-sh `dirname $0`/../fprintkill "$@" >>/tmp/fprintkill-watch.$$.log 2>&1 &
-watch -n1 "(echo $$ ; pgrep -x fprintkill ; pgrep -x fprintd-verify ; pgrep '$1') | xargs ps -oppid,pid,pgid,cmd ; cat /tmp/fprintkill-watch.$$.log "
