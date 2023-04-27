f311af0d1cd386a0e201acf527bc08745ef56e50
fraxflac <frax@axnet.nu>
2023-04-27 13:46:54 +0200

    grep full cmdline

--- debug/watch-fprintkill.sh
+++ debug/watch-fprintkill.sh
@@ -3,6 +3,6 @@
 # example usage:
 # ../fprintkill sh -c 'sleep 111 & sleep 222 & sleep 333 &' & ./watch-fprintkill.sh sleep 
 
-PGREP='pgrep -x fprintkill ; pgrep -x fprintd-verify'
+PGREP='pgrep -f fprintkill ; pgrep -x fprintd-verify'
 for c in $@; do PGREP="$PGREP ; pgrep $c"; done
 watch -n1 "(echo $$ ; eval \"$PGREP\") | xargs ps -oppid,pid,pgid,cmd "
