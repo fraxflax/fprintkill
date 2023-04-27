793603343bb43bffce66558d33bd4ac2b94d1676
fraxflac <frax@axnet.nu>
2023-04-27 09:10:34 +0200

    rm logfile

--- debug/watch-cmd-fprintkill.sh
+++ debug/watch-cmd-fprintkill.sh
@@ -13,5 +13,5 @@ export FPK_ARGS
 echo ~~~ /tmp/fprintkill-watch.$$.log ~~~ > /tmp/fprintkill-watch.$$.log
 ( while ps $$ >/dev/null 2>&1; do sleep 1; done ; rm -v /tmp/fprintkill-watch.$$.log ) &
 sh `dirname $0`/../fprintkill >>/tmp/fprintkill-watch.$$.log 2>&1 &
-watch -n1 "(echo $$ ; pgrep fprintkill ; pgrep fprintd-verify ; pgrep '$1') | xargs ps -oppid,pid,pgid,cmd ; cat /tmp/fprintkill-watch.$$.log "
+watch -n1 "(echo $$ ; pgrep -x fprintkill ; pgrep -x fprintd-verify ; pgrep '$1') | xargs ps -oppid,pid,pgid,cmd ; cat /tmp/fprintkill-watch.$$.log "
 
